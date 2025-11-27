1cd2557899caf7fc001d4f48647e9ee77efefc6e
Joao Gabriel Amaral de Sales <joaogabrielamaralsales@gmail.com>
2025-11-26 23:14:32 -0300

    Update 1165.py

--- 1165.py
+++ 1165.py
@@ -1,22 +1,20 @@
-import math
+n = int(input())
 
-N = int(input())
+def primo(x):
+    d = 2
+    conta_div = 0
 
-for _ in range(N):
-    X = int(input())
-    if X < 2:
-        print(f"{X} nao eh primo")
-        continue
+    while d * d <= x:
+        conta_div += (x % d == 0)
+        d += 1
 
-    eh_primo = True
-    limite = int(math.sqrt(X)) + 1
+    return {
+        True:  f"{x} nao eh primo",
+        False: f"{x} eh primo"
+    }[conta_div > 0]
 
-    for i in range(2, limite):
-        if X % i == 0:
-            eh_primo = False
-            break
-
-    if eh_primo:
-        print(f"{X} eh primo")
-    else:
-        print(f"{X} nao eh primo")
+i = 0
+while i < n:
+    x = int(input())
+    print(primo(x))
+    i += 1
